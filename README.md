5c25a255589c858422895df614f473a206fb4f6e
javier-vidalruano <67796321+javiervidrua@users.noreply.github.com>
2023-02-05 21:41:29 +0100

    Added the street name to each street's marker

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # ORA-map
 A simple webpage that shows the streets available to park inside the Zone B for the ORA of Salamanca.
 
-## TODO
-- [ ] Show the name of the address when you click on the marker.
+## TX] Show the name of the address when you click on the marker.
--- process-addresses-geolocation.js
+++ process-addresses-geolocation.js
@@ -1,5 +1,5 @@
 let geolocations = require('./addresses_geolocation_v2')
 
 for (let location in geolocations) {
-    console.log(`[${geolocations[location][0].lat},${geolocations[location][0].lon}],`)
+    console.log(`[${geolocations[location][0].lat},${geolocations[location][0].lon},"${geolocations[location][0].display_name}"],`)
 }
