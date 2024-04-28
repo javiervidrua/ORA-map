8072d83edfee2169dbd50ee01c6e4fa3ccd873d1
javier-vidalruano <67796321+javiervidrua@users.noreply.github.com>
2024-04-28 17:11:41 +0200

    Improved the algorithm to process a higher % of correct streets

--- process-addresses-geolocation.js
+++ process-addresses-geolocation.js
@@ -1,5 +1,17 @@
-let geolocations = require('./addresses_geolocation_v2')
+let geolocations = require('./geolocations')
 
 for (let location in geolocations) {
-    console.log(`[${geolocations[location][0].lat},${geolocations[location][0].lon},"${geolocations[location][0].display_name}"],`)
+    // If no possibility contains a zip code, we take the first one
+    if (!JSON.stringify(geolocations[location]).includes(", 370")) {
+        console.log(`[${geolocations[location][0].lat},${geolocations[location][0].lon},"${geolocations[location][0].display_name}"],`)
+    }
+    // Otherwise, we check each one
+    else {
+        for (let possibility in geolocations[location]) {
+            if (geolocations[location][possibility].display_name.includes(", 370")) {
+                console.log(`[${geolocations[location][possibility].lat},${geolocations[location][possibility].lon},"${geolocations[location][possibility].display_name}"],`)
+                break
+            }
+        }
+    }
 }
